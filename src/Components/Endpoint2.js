631f7cef0571e7b54e0303e01149fadb488b014c
JonathanPJuhl <cph-jj450@cphbusiness.dk>
2021-05-06 16:52:21 +0200

    Still working on pinned

--- src/Components/Endpoint2.js
+++ src/Components/Endpoint2.js
@@ -1,82 +1,88 @@
-
-import '../App.css';
-import React, {useState, useEffect} from 'react';
-import {fetchUsername} from "./decodeJWT"
-import {pinnedStocks} from "../settings";
+import "../App.css";
+import React, { useState, useEffect } from "react";
+import { fetchUsername } from "./decodeJWT";
+import { pinnedStocks } from "../settings";
 import * as ReactBootStrap from "react-bootstrap";
-let arr = [];
-function Endpoint2() {
 
+function Endpoint2() {
+  
   useEffect(() => {
-fetchItems().then(keepfetching());
+    fetchItems().then(keepfetching());
   }, []);
 
-  const [pinned, setPinned] = useState ([]);
-  const [item, setItems] = useState ([]);
+  const [pinned, setPinned] = useState([]);
+  const [item, setItems] = useState([]);
+  let arr = [];
 
-function keepfetching(){
-for(let i=0; i<pinns.length-1; i++){
-  fetchInfo(pinns[i]);
-}}
-const fetchItems =  async () => { 
-  const data = await fetch(`${pinnedStocks}${fetchUsername()}`);
+  function keepfetching() {
+   
+    pinned.map(function  (e){
+      console.log(e.stockticker)
+      return fetchInfo(e.stockTicker) 
+    })
+    console.log(arr);
+    setItems(arr);
+    console.log("item" + Object.values(item))
+    // for (let i = 0; i < pinned.length-1; i++) {
+      
+    //   if (i % 4 == 0) {
+    //     setTimeout(() => {
+    //       fetchInfo(pinned[i].stockTicker);
+    //     }, 3000);
+    //   } else {
+    //     console.log(pinned[i].stockTicker);
+    //     fetchInfo(pinned[i].stockTicker);
+    //   }
+    // }
+  };
 
-  const pins = await data.json();
-  setPinned(pins);
+  const fetchInfo = async (ticker) => {
+    const data = await fetch(
+      `https://api.marketstack.com/v1/eod/latest?access_key=5feeee1a869fedc6e6e24e62c735bc22&symbols=${ticker}`
+    );
 
+    const items = await data.json();
+    console.log(Object.values(items.data));
+    console.log("symsym" + items.data[0].symbol);
+    arr.push(items.data[0]);
+    //setItems(...item, items.data);
+    
+  };
 
-
-let pinns = pinned.map(function(e){
-        
-  return e.stockTicker;
-})
-
-
-
-const fetchInfo =  async (ticker) => { 
-  const data = await fetch(`https://api.marketstack.com/v1/eod/latest?access_key=5feeee1a869fedc6e6e24e62c735bc22&symbols=${ticker}`
-  );
-
-  const items = await data.json();
-     //console.log(items.data);
-    // setItems(items.data);
-    arr.push(items.data)
-}
-
-console.log("array: " + item)
-setItems(arr);
-//console.log("done" + JSON.stringify(item[0].symbol))
-}
-  return (
+  const fetchItems = async () => {
+    const data = await fetch(`${pinnedStocks}${fetchUsername()}`);
+    console.log(`URL: ${pinnedStocks}${fetchUsername()}`)
+    const pins = await data.json();
+    setPinned( pins);
+  };
   
+  return (
     <div>
-       
-       <ReactBootStrap.Table striped bordered hover variant="sm" >
-    <thead>
-<tr>
+      <ReactBootStrap.Table striped bordered hover variant="sm">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Date</th>
+            <th>Open</th>
+            <th>Low</th>
+            <th>High</th>
+            <th>Close</th>
 
-  <th>Name</th>
-  <th>Tickers</th>
-  <th>Add to pinned</th>
-</tr>
-</thead>
+          </tr>
+        </thead>
 
-
-        {item.map(item => (
-         <tbody key={item.high}>
-            
-         
+        {item.map((item) => (
+          <tr key={item.low}>
             <td>{item.symbol}</td>
-            
-        </tbody>
- )) }
-
-
-
-</ReactBootStrap.Table> 
-
-
-</div>
+            <td>{item.date}</td>
+            <td>{item.open}</td>
+            <td>{item.low}</td>
+            <td>{item.high}</td>
+            <td>{item.close}</td>
+          </tr>
+        ))}
+      </ReactBootStrap.Table>
+    </div>
   );
 }
 
