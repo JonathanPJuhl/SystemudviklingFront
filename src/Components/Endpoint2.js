b849005bb108d24b43095494131dd0af8c222c5e
JonathanPJuhl <cph-jj450@cphbusiness.dk>
2021-05-10 22:57:06 +0200

    showing table of pinned stocks finally works

--- src/Components/Endpoint2.js
+++ src/Components/Endpoint2.js
@@ -12,51 +12,21 @@ function Endpoint2() {
   }, []);
 
   const [pinned, setPinned] = useState([]);
-  // const [item, setItems] = useState([]);
-  // let arr = [];
 
-  // function keepfetching() {
-   
-  //   pinned.map(function  (e){
-  //     console.log(e.stockticker)
-  //      fetchInfo(e.stockTicker) 
-  //   })
-  //   console.log(arr);
-  //   setItems(arr);
-  //   console.log("item" + Object.values(item))
 
-  // };
-
-  // const fetchInfo = async (ticker) => {
-  //   const data = await fetch(
-  //     `https://api.marketstack.com/v1/eod/latest?access_key=5feeee1a869fedc6e6e24e62c735bc22&symbols=${ticker}`
-  //   );
-
-  //   const items = await data.json();
-  //   console.log(Object.values(items.data));
-  //   console.log("symsym" + items.data[0].symbol);
-  //   arr.push(items.data[0]);
-  //   //setItems(...item, items.data[0]);
-    
-  // };
-let arr = [];
   const fetchItems = async () => {
     const data = await fetch(`${pinnedStocks}${fetchUsername()}`);
     //console.log(`URL: ${pinnedStocks}${fetchUsername()}`)
     const pins = await data.json();
 
-     arr = pins.map(function (e){
-     // console.log("PINNED: " + e.data[0]);
-      return e.data[0]
-    })
-    //setPinned(...arr);
+   
+    setPinned(pins)
     
   };
-  //console.log("pinned bf return: " + Object.values(pinned));
- // console.log("item bf return: " + Object.values(item));
-// console.log("arr bf return: " + Object.values(arr));
+
 
   return (
+    
     <div>
       <ReactBootStrap.Table striped bordered hover variant="sm">
         <thead>
@@ -71,15 +41,15 @@ let arr = [];
           </tr>
         </thead>
        
-        {console.log(arr)}
-         {arr.map((item) => (
-          <tr key={item.low}>
-            <td>{item.symbol}</td>
-            <td>{item.date}</td>
-            <td>{item.open}</td>
-            <td>{item.low}</td>
-            <td>{item.high}</td>
-            <td>{item.close}</td>
+      
+         {pinned.map((item) => (
+          <tr key={item.data[0].symbol}>
+            <td>{item.data[0].symbol}</td>
+            <td>{item.data[0].date}</td>
+            <td>{item.data[0].open}</td>
+            <td>{item.data[0].low}</td>
+            <td>{item.data[0].high}</td>
+            <td>{item.data[0].close}</td>
           </tr>
         ))} 
       </ReactBootStrap.Table>
